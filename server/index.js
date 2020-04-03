b77aac878f35d0c870a1a4d9aeb4b9b00c36308a
Aliya Akmatova <amavarati@gmail.com>
2020-04-03 18:30:02 +0600

    Fixed error

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const expressWs = require('express-ws');
 const {nanoid} = require('nanoid');
 
